e39266abf49de3712aa381ae37a0d702e499a449
Szymon Józefowicz <szymon@statoilfuelretail.com>
2019-07-10 14:00:31 +0200

    Create sample.py

---
+++ code/dags/sample.py
@@ -0,0 +1,37 @@
+from datetime import timedelta
+
+import airflow
+from airflow.models import DAG
+from airflow.operators.bash_operator import BashOperator
+from airflow.operators.dummy_operator import DummyOperator
+
+args = {
+    'owner': 'airflow',
+    'start_date': airflow.utils.dates.days_ago(2),
+}
+
+dag = DAG(
+    dag_id='example_bash_operator',
+    default_args=args,
+    schedule_interval='0 0 * * *',
+    dagrun_timeout=timedelta(minutes=60),
+)
+
+start = DummyOperator(
+    task_id='start',
+    dag=dag,
+)
+
+vm_run = BashOperator(
+    task_id='vm_run',
+    bash_command='az --help ',
+    dag=dag,
+)
+
+end = DummyOperator(
+    task_id='end',
+    dag=dag,
+)
+
+start >> vm_run >>stop
+
